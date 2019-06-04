35b9242bd1de0194d555d0e63450681bcb511639
кирилл Захаров <csgo2003@mail.ru>
2019-06-04 18:21:11 +0600

    initial

---
+++ TaskManager.Services/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Services
+{
+    public class EmailSender
+    {
+
+        public void SendMail(string fromMail, string password, string fromName, string toMail, string theme, string text)
+        {
+            try
+            {
+                MailAddress from = new MailAddress(fromMail, fromName);
+                MailAddress to = new MailAddress(toMail);
+
+                MailMessage mail = new MailMessage(from, to);
+                mail.Subject = theme;
+                mail.Body = text;
+
+                string smtpHost;
+                if (fromMail.Contains("gmail.com"))
+                {
+                    smtpHost = "smtp.gmail.com";
+                }
+                else if (fromMail.Contains("mail.ru")) 
+                {
+                    smtpHost = "smtp.mail.ru";
+                }
+                else
+                {
+                    throw new Exception();
+                }
+                SmtpClient smtp = new SmtpClient(smtpHost, 587);
+                smtp.Credentials = new NetworkCredential(fromMail, password);
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ TaskManager.Services/FileDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Services
+{
+    public class FileDownloader
+    {
+        public void DownloadFile(string filePath, string fileName)
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadFile(filePath, fileName);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
