dc066806ebcece2c56a82b98823d2512260b055b
Шадикьян Руслан Артемович <@corp.itstep>
2019-06-06 18:20:38 +0600

    initial

---
+++ TaskManager.Services/DirectoryChanger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Services
+{
+    public class DirectoryChanger
+    {
+        public void ChangeDirectory(string from, string to)
+        {
+            DirectoryInfo destDir = new DirectoryInfo(to);
+            if (destDir.Exists)
+                destDir.Delete(true);
+            new DirectoryInfo(from).MoveTo(to);
+        }
+    }
+}
--- TaskManagerApp/MainWindow.xaml.cs
+++ TaskManagerApp/MainWindow.xaml.cs
@@ -23,6 +23,16 @@ namespace TaskManagerApp
         public MainWindow()
         {
             InitializeComponent();
+            taskTypeComboBox.Items.Add("Рассылка email");
+            taskTypeComboBox.Items.Add("Скачивание файла");
+            taskTypeComboBox.Items.Add("Перемещение файлов одного каталога в новый каталог");
+            taskTypeComboBox.SelectedIndex = 0;
+
+            periodicityComboBox.Items.Add("Раз в день");
+            periodicityComboBox.Items.Add("Раз в неделю");
+            periodicityComboBox.Items.Add("Раз в месяц");
+            periodicityComboBox.Items.Add("Раз в год");
+            periodicityComboBox.SelectedIndex = 0;
         }
 
         private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -30,6 +40,76 @@ namespace TaskManagerApp
             WindowState = WindowState.Minimized;
             ShowInTaskbar = false;
             e.Cancel = true;
+        }
+
+        private void TaskTypeComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (taskTypeComboBox.SelectedIndex == 0)
+            {
+                emailSendingGrid.Visibility = Visibility.Visible;
+                emailSendingGrid.IsEnabled = true;
+
+                fileDownloadingGrid.Visibility = Visibility.Hidden;
+                fileDownloadingGrid.IsEnabled = false;
+
+                directoryChangingGrid.Visibility = Visibility.Hidden;
+                directoryChangingGrid.IsEnabled = false;
+            }
+            else if (taskTypeComboBox.SelectedIndex == 1)
+            {
+                emailSendingGrid.Visibility = Visibility.Hidden;
+                emailSendingGrid.IsEnabled = false;
+
+                fileDownloadingGrid.Visibility = Visibility.Visible;
+                fileDownloadingGrid.IsEnabled = true;
+
+                directoryChangingGrid.Visibility = Visibility.Hidden;
+                directoryChangingGrid.IsEnabled = false;
+            }
+            else
+            {
+                emailSendingGrid.Visibility = Visibility.Hidden;
+                emailSendingGrid.IsEnabled = false;
+
+                fileDownloadingGrid.Visibility = Visibility.Hidden;
+                fileDownloadingGrid.IsEnabled = false;
+
+                directoryChangingGrid.Visibility = Visibility.Visible;
+                directoryChangingGrid.IsEnabled = true;
+            }
+        }
+
+        private void AddTask(object sender, RoutedEventArgs e)
+        {
+            if (datePicker.SelectedDate == null || datePicker.SelectedDate < DateTime.Now)
+            {
+                MessageBox.Show("Некорректная дата");
+                return;
+            }
+            if (timePicker.SelectedTime == null)
+            {
+                MessageBox.Show("Некорректное время");
+                return;
+            }
+
+            if (taskTypeComboBox.SelectedIndex == 0)
+            {
+                
+            }
+            else if (taskTypeComboBox.SelectedIndex == 1)
+            {
+
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(directoryFromLabel.Text))
+                {
+                    MessageBox.Show("Введите путь до папки");
+                }
+                if (string.IsNullOrEmpty(directoryToLabel.Text))
+                {
+                    MessageBox.Show("Введите путь до папки");
+                }
         }
     }
 }
